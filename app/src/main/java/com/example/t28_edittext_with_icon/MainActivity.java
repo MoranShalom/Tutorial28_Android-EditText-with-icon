6c227ecf6e236c56351d79de630eec0810fecfed
MoranShalom <moranshalom1234@gmail.com>
2019-04-12 02:50:44 +0300

    EditText with icon

---
+++ app/src/main/java/com/example/t28_edittext_with_icon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t28_edittext_with_icon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
